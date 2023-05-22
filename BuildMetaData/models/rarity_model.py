71c4bec38d02c4f86bd9765a275dfd4a9975c504
Max Schneider <ms.matix@googlemail.com>
2023-05-22 10:18:28 +0200

    fix(rarity): do not store a json -> enum for ts

--- BuildMetaData/models/rarity_model.py
+++ BuildMetaData/models/rarity_model.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 from dataclasses import dataclass
 
 from ..common import FILE_RARITY
@@ -14,30 +15,62 @@ class RarityMetaModel:
 
     @staticmethod
     def generate_init_data():
-        rarity_dict = {}
+        # rarity_dict = {}
+        init_data = ""
         for enum_value in ERarity:
             # generate json sections which store a dict
-            rarity_dict[enum_value.name] = list([])
+            # rarity_dict[enum_value.value] = list([])
+            # init_data += f"{enum_value.value}{{\n}}\n"
+            init_data += f"export enum {enum_value.value}{{\n}}\n"
 
         with open(FILE_RARITY, "w") as f:
-            json.dump(rarity_dict, f)
+            # json.dump(rarity_dict, f)
+            f.write(init_data)
 
     @staticmethod
-    def nft_name_available(entry, file):
-        return all(entry not in file[enum_value.name] for enum_value in ERarity)
+    # def nft_name_available(entry, file):
+    def nft_name_available(entry, input):
+        res = all(entry not in input[enum_value.value] for enum_value in ERarity)
+        return res
 
     @staticmethod
     def append_data(FILE_RARITY, rarity, new_data):
-        with open(FILE_RARITY, "r+") as f:
-            data = json.load(f)
-            if RarityMetaModel.nft_name_available(new_data, data):
-                data[ERarity(rarity).name].append(dict({new_data: new_data}))
-                f.seek(0)
-                # json.dump(data, f, indent=2)
-                json.dump(data, f, indent=2)
-                return True
-            else:  # pragma no cover
-                raise NFTAlreadyExist("NFT name already awarded.")
+        data_input = {}
+        current_section = None
+        # read file and store in a dict
+        with open(FILE_RARITY, "r") as f:
+            # data = json.load(f)
+            for line in f:
+                line = line.strip()
+
+                if line.endswith("{"):
+                    words = line.split()
+                    modified_line = " ".join(words[2:])
+                    current_section = modified_line[:-1]
+                    data_input[current_section] = list([])  # war {}
+                elif line.endswith("}"):
+                    current_section = None
+                elif line:
+                    key, value = line.split("=")
+                    key = key.strip()
+                    value = value.strip().strip('"')
+                    if current_section:
+                        data_input[current_section].append({key: value})
+
+        if RarityMetaModel.nft_name_available(new_data, data_input):
+            data_input[ERarity(rarity).value].append(dict({new_data: new_data}))
+            # f.seek(0)
+            # json.dump(data, f, indent=2)
+            with open(FILE_RARITY, "w") as f:
+                for section, items in data_input.items():
+                    f.write(f"export enum {section}{{\n")
+                    for item in items:
+                        for key, value in item.items():
+                            f.write(f'    {key} = "{value}"\n')
+                    f.write("}\n")
+            return True
+        else:  # pragma no cover
+            raise NFTAlreadyExist("NFT name already awarded.")
 
     def save(self):
         if not os.path.isfile(FILE_RARITY):
